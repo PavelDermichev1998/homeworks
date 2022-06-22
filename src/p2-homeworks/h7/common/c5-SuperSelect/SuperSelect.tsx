c8db29fb41d6b840dfc6071f2a36e77689fbe492
PavelDermichev1998 <pdermichev@gmail.com>
2022-06-22 21:03:15 +0300

    hw7

--- src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
+++ src/p2-homeworks/h7/common/c5-SuperSelect/SuperSelect.tsx
@@ -1,4 +1,5 @@
 import React, {SelectHTMLAttributes, DetailedHTMLProps, ChangeEvent} from 'react'
+import style from './SuperSelect.module.css'
 
 type DefaultSelectPropsType = DetailedHTMLProps<SelectHTMLAttributes<HTMLSelectElement>, HTMLSelectElement>
 
@@ -14,14 +15,17 @@ const SuperSelect: React.FC<SuperSelectPropsType> = (
         ...restProps
     }
 ) => {
-    const mappedOptions: any[] = []; // map options with key
+    const mappedOptions: any[] = options ? options.map((o, i) => (
+        <option className={style.option} key={i}>{o}</option>
+    )) : []; // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
     return (
-        <select onChange={onChangeCallback} {...restProps}>
+        <select onChange={onChangeCallback} className={style.select} {...restProps}>
             {mappedOptions}
         </select>
     )
