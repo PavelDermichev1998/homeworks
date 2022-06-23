c4e16cac7490a936ae870c52826627032e18175a
PavelDermichev1998 <pdermichev@gmail.com>
2022-06-23 22:39:31 +0300

    hw10

--- src/p2-homeworks/h10/bll/loadingReducer.ts
+++ src/p2-homeworks/h10/bll/loadingReducer.ts
@@ -1,14 +1,22 @@
 const initState = {
-
+    isLoading: false
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: ActionType): { isLoading: boolean } => { // fix any
     switch (action.type) {
-        case '': {
-            return state
+        case 'SET-LOADING': {
+            return {...state, isLoading: action.loading}
         }
-        default: return state
+        default:
+            return state
     }
 }
 
-export const loadingAC = (): any => {} // fix any
+export const loadingAC = (loading: boolean): ActionType => {
+    return {type: 'SET-LOADING', loading}
+}
+
+type ActionType = {
+    type: 'SET-LOADING',
+    loading: boolean
+}
