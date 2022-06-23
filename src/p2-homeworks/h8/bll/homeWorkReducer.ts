4b720095d333a3b8810e95328e126253f80c3fc3
PavelDermichev1998 <pdermichev@gmail.com>
2022-06-23 19:39:42 +0300

    hw8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,19 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from "../HW8";
+
+export const homeWorkReducer = (state: Array<UserType>, action: { type: string, payload: string | number }): any => {
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            const stateCopy = [...state]
+            if (action.payload === 'up') {
+                return stateCopy.sort((a, b) => a.age < b.age ? 1 : -1)
+            } else {
+                return stateCopy.sort((a, b) => a.age > b.age ? 1 : -1)
+            }
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(p => p.age > action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,7 +1,7 @@
-import React from 'react'
 import {homeWorkReducer} from '../homeWorkReducer'
+import {UserType} from "../../HW8";
 
-let initialState: any[] // need to fix any
+let initialState: UserType[]
 
 beforeEach(() => {
     initialState = [
@@ -16,17 +16,20 @@ beforeEach(() => {
 
 test('sort name up', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
-
-    console.log(newState)
-    // expect(...).toBe(...)
+    expect(newState.length).toBe(6)
+    expect(newState[0]._id).toBe(1)
+    expect(newState[5]._id).toBe(0)
 })
 test('sort name down', () => {
     const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
-
+    expect(newState.length).toBe(6)
+    expect(newState[0]._id).toBe(0)
+    expect(newState[5]._id).toBe(1)
 
 })
 test('check age 18', () => {
     const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
-
-
+    expect(newState.length).toBe(4)
+    expect(newState[0]._id).toBe(1)
+    expect(newState[3]._id).toBe(5)
 })
